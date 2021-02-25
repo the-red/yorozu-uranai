2706b98ae46888421456db832547321b4a4e9732
funady <funada@sonicgarden.jp>
2021-02-25 14:29:26 +0900

    定数Tの計算を本実装した

--- src/calculateT.ts
+++ src/calculateT.ts
@@ -1,3 +1,9 @@
-export const calculateT = () => {
-  return 14.917
+import { DateTime } from 'luxon'
+
+export const calculateT = (_birthday: Date) => {
+  const birthday = DateTime.fromJSDate(_birthday)
+  const hour = birthday.hour
+  const minute = birthday.minute
+  const T = hour + minute / 60
+  return Math.round(T * 1000) / 1000
 }
--- test/calculateT.test.ts
+++ test/calculateT.test.ts
@@ -2,7 +2,7 @@ import { calculateT } from '../src/calculateT'
 
 describe('calculateT', () => {
   it('14：55をTに変換', () => {
-    const T = calculateT()
+    const T = calculateT(new Date('1979-08-06 14:55:00'))
     expect(T).toEqual(14.917)
   })
 })

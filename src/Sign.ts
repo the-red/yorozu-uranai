b62e52df78e2531af00401eaf5a3c1440e994dff
funady <funada@sonicgarden.jp>
2021-03-10 00:27:30 +0900

    ZodiacSign,StarPositionを全て記述

--- src/Sign.ts
+++ src/Sign.ts
@@ -1,10 +1,30 @@
 import { ZoneOptions } from 'luxon'
 
-type ZodiacSign = '牡羊座' | '牡牛座'
+type ZodiacSign =
+  | '牡羊座'
+  | '牡牛座'
+  | '双子座'
+  | '蟹座'
+  | '獅子座'
+  | '乙女座'
+  | '天秤座'
+  | '蠍座'
+  | '射手座'
+  | '山羊座'
+  | '水瓶座'
+  | '魚座'
 
 type StarPosition = {
   sun: ZodiacSign
   moon: ZodiacSign
+  mercury: ZodiacSign
+  venus: ZodiacSign
+  mars: ZodiacSign
+  jupiter: ZodiacSign
+  saturn: ZodiacSign
+  uranus: ZodiacSign
+  neptune: ZodiacSign
+  pluto: ZodiacSign
 }
 
 type Star = keyof StarPosition

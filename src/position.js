d2c70fe3647f637aae05ecfb7388280b42cbae1d
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-03-09 10:21:51 +0900

    refactor: ガード節に変更

--- src/position.js
+++ src/position.js
@@ -23,30 +23,28 @@ function ParseDate(text) {
 }
 
 function Demo() {
-  if (process.argv.length === 5) {
-    const latitude = ParseNumber(process.argv[2], 'latitude')
-    const longitude = ParseNumber(process.argv[3], 'longitude')
-    const observer = new Observer(latitude, longitude, 0)
-    const date = process.argv.length === 5 ? ParseDate(process.argv[4]) : new Date()
-    console.log(`UTC date = ${date.toISOString()}`)
-    console.log('')
-    console.log(`${'BODY'.padEnd(8)} ${'SIGN'} ${'DEGREES'.padStart(8)} ${'ELON'.padStart(8)} ${'DEC'.padStart(8)}`)
-    for (let body of ['Sun', 'Moon', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']) {
-      const vector = GeoVector(body, date, true)
-      const ecliptic = Ecliptic(vector.x, vector.y, vector.z)
-      const equator = Equator(body, date, observer, false, true)
-      const sign = Math.trunc(ecliptic.elon / 30)
-      const degrees = ecliptic.elon - sign * 30
-      console.log(
-        `${body.padEnd(8)} ${String(sign).padStart(4)} ${Format(degrees)} ${Format(ecliptic.elon)} ${Format(
-          equator.dec
-        )}`
-      )
-    }
-    process.exit(0)
+  if (process.argv.length < 5) {
+    console.log('USAGE: node positions.js latitude longitude date')
+    process.exit(1)
   }
-  console.log('USAGE: node positions.js latitude longitude date')
-  process.exit(1)
+
+  const latitude = ParseNumber(process.argv[2], 'latitude')
+  const longitude = ParseNumber(process.argv[3], 'longitude')
+  const observer = new Observer(latitude, longitude, 0)
+  const date = ParseDate(process.argv[4])
+  console.log(`UTC date = ${date.toISOString()}`)
+  console.log('')
+  console.log(`${'BODY'.padEnd(8)} ${'SIGN'} ${'DEGREES'.padStart(8)} ${'ELON'.padStart(8)} ${'DEC'.padStart(8)}`)
+  for (let body of ['Sun', 'Moon', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']) {
+    const vector = GeoVector(body, date, true)
+    const ecliptic = Ecliptic(vector.x, vector.y, vector.z)
+    const equator = Equator(body, date, observer, false, true)
+    const sign = Math.trunc(ecliptic.elon / 30)
+    const degrees = ecliptic.elon - sign * 30
+    console.log(
+      `${body.padEnd(8)} ${String(sign).padStart(4)} ${Format(degrees)} ${Format(ecliptic.elon)} ${Format(equator.dec)}`
+    )
+  }
 }
 
 Demo()

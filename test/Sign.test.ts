f75dde3be42b543e061ca9dd398002c963dffaa2
funady <funada@sonicgarden.jp>
2021-03-12 18:00:16 +0900

    テストコード修正

--- test/Sign.test.ts
+++ test/Sign.test.ts
@@ -1,7 +1,18 @@
 import { Sign } from '../src/Sign'
 
 describe('Sign', () => {
-  const sign = new Sign({ sun: '牡羊座', moon: '牡牛座' })
+  const sign = new Sign({
+    sun: '乙女座',
+    moon: '魚座',
+    mercury: '天秤座',
+    venus: '乙女座',
+    mars: '乙女座',
+    jupiter: '牡羊座',
+    saturn: '射手座',
+    uranus: '射手座',
+    neptune: '山羊座',
+    pluto: '蠍座',
+  })
   it('火', () => {
     expect(sign.fire()).toEqual(['sun'])
   })

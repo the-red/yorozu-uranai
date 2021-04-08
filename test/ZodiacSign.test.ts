5f5e8cfd39ebb77ad7e7beea858426ba2f743ed4
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-04-08 15:22:59 +0900

    test: テスト追加 0.0のとき魚座には現状ならない

--- test/ZodiacSign.test.ts
+++ test/ZodiacSign.test.ts
@@ -1,12 +1,22 @@
 import { CZodiacSign } from '../src/ZodiacSign'
 
 describe('ZodiacSign', () => {
-  it('60', () => {
+  it('0.0: 魚座', () => {
+    const zodiacSign = new CZodiacSign(0)
+    expect(zodiacSign.sign).toEqual('魚座')
+    expect(zodiacSign.degrees).toEqual(0)
+  })
+  it('0.5: 牡羊座', () => {
+    const zodiacSign = new CZodiacSign(0.5)
+    expect(zodiacSign.sign).toEqual('牡羊座')
+    expect(zodiacSign.degrees).toEqual(0.5)
+  })
+  it('60.0: 牡牛座', () => {
     const zodiacSign = new CZodiacSign(60)
     expect(zodiacSign.sign).toEqual('牡牛座')
     expect(zodiacSign.degrees).toEqual(0)
   })
-  it('61', () => {
+  it('61.89: 双子座', () => {
     const zodiacSign = new CZodiacSign(61.89)
     expect(zodiacSign.sign).toEqual('双子座')
     expect(zodiacSign.degrees).toEqual(1.8900000000000006)

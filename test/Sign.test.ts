794b732711d360b33791a0413c7bfd18dd853872
funady <funada@sonicgarden.jp>
2021-04-08 13:57:05 +0900

    テストを種類ごとにグループ化した

--- test/Sign.test.ts
+++ test/Sign.test.ts
@@ -13,22 +13,26 @@ describe('Sign', () => {
     neptune: '山羊座',
     pluto: '蠍座',
   })
-  it('火', () => {
-    expect(sign.fire()).toEqual(['jupiter', 'saturn', 'uranus'])
+  describe('Element', () => {
+    it('火', () => {
+      expect(sign.fire()).toEqual(['jupiter', 'saturn', 'uranus'])
+    })
+    it('土', () => {
+      expect(sign.earth()).toEqual(['sun', 'venus', 'mars', 'neptune'])
+    })
+    it('風', () => {
+      expect(sign.air()).toEqual(['mercury'])
+    })
+    it('水', () => {
+      expect(sign.water()).toEqual(['moon', 'pluto'])
+    })
   })
-  it('土', () => {
-    expect(sign.earth()).toEqual(['sun', 'venus', 'mars', 'neptune'])
-  })
-  it('風', () => {
-    expect(sign.air()).toEqual(['mercury'])
-  })
-  it('水', () => {
-    expect(sign.water()).toEqual(['moon', 'pluto'])
-  })
-  it('男性', () => {
-    expect(sign.masculine()).toEqual(['jupiter', 'saturn', 'uranus', 'mercury'])
-  })
-  it('女性', () => {
-    expect(sign.feminine()).toEqual(['sun', 'venus', 'mars', 'neptune', 'moon', 'pluto'])
+  describe('Polarity', () => {
+    it('男性', () => {
+      expect(sign.masculine()).toEqual(['jupiter', 'saturn', 'uranus', 'mercury'])
+    })
+    it('女性', () => {
+      expect(sign.feminine()).toEqual(['sun', 'venus', 'mars', 'neptune', 'moon', 'pluto'])
+    })
   })
 })

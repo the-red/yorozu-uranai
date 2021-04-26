a403665a86bb54719f5c371277b6b3a2696fa0e7
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-04-26 15:31:09 +0900

    refactor: 不要なクラス・関数を削除

--- src/calculateT.ts
+++
@@ -1,9 +0,0 @@
-import { DateTime } from 'luxon'
-
-export const calculateT = (_birthday: Date) => {
-  const birthday = DateTime.fromJSDate(_birthday)
-  const hour = birthday.hour
-  const minute = birthday.minute
-  const T = hour + minute / 60
-  return Math.round(T * 1000) / 1000
-}
--- test/calculateT.test.ts
+++
@@ -1,8 +0,0 @@
-import { calculateT } from '../src/calculateT'
-
-describe('calculateT', () => {
-  it('14：55をTに変換', () => {
-    const T = calculateT(new Date('1979-08-06 14:55:00'))
-    expect(T).toEqual(14.917)
-  })
-})

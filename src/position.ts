d5ef116c6ef5ced9af807e06005fd839949fa289
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-05-01 23:42:21 +0900

    refactor: TypeScript対応

--- src/position.ts
+++ src/position.ts
@@ -1,18 +1,19 @@
-const { strict: assert } = require('assert')
-const swisseph = require('swisseph')
+import { strict as assert } from 'assert'
+// @ts-ignore
+import swisseph from 'swisseph'
 
-const planets = [
-  'SUN',
-  'MOON',
-  'MERCURY',
-  'VENUS',
-  'MARS',
-  'JUPITER',
-  'SATURN',
-  'URANUS',
-  'NEPTUNE',
-  'PLUTO',
-]
+type EclipticPosition = {
+  longitude: number
+  latitude: number
+  distance: number
+  longitudeSpeed: number
+  latitudeSpeed: number
+  distanceSpeed: number
+  rflag: number
+  error?: any
+}
+
+const planets = ['SUN', 'MOON', 'MERCURY', 'VENUS', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO'] as const
 
 // Test date
 const year = 1987
@@ -24,13 +25,13 @@ const offset = 9
 console.log('Test date:', { year, month, day, hour, minutes, offset })
 
 // Julian day
-swisseph.swe_julday(year, month, day, hour + minutes / 60 - offset, swisseph.SE_GREG_CAL, function (julday_ut) {
+swisseph.swe_julday(year, month, day, hour + minutes / 60 - offset, swisseph.SE_GREG_CAL, (julday_ut: number) => {
   assert.equal(julday_ut, 2447046.4951388887)
   console.log('Julian UT day for date:', julday_ut)
 
   planets.forEach((planet) => {
     // position
-    swisseph.swe_calc_ut(julday_ut, swisseph[`SE_${planet}`], swisseph.SEFLG_SPEED, function (body) {
+    swisseph.swe_calc_ut(julday_ut, swisseph[`SE_${planet}`], swisseph.SEFLG_SPEED, (body: EclipticPosition) => {
       assert(!body.error, body.error)
       const fullDegrees = body.longitude
 
@@ -42,9 +43,9 @@ swisseph.swe_julday(year, month, day, hour + minutes / 60 - offset, swisseph.SE_
       const degreesMinInt = Math.trunc(degreesMin)
       const degreesSec = (degreesMin - degreesMinInt) * 60
       const degreesSecInt = Math.trunc(degreesSec)
-      const degreesToPrint = `${String(degreesInt).padStart(2)}°${String(degreesMinInt).padStart(2, 0)}′${String(
+      const degreesToPrint = `${String(degreesInt).padStart(2)}°${String(degreesMinInt).padStart(2, '0')}′${String(
         degreesSecInt
-      ).padStart(2, 0)}″`
+      ).padStart(2, '0')}″`
       console.log(`${planet}:`, { sign, degrees: degreesToPrint })
     })
   })

d2a267aeb826e654f6dfd8d879e63b4c316b4bbc
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-05-02 01:08:02 +0900

    refactor: position取得部と表示部で別ループに

--- src/position.ts
+++ src/position.ts
@@ -18,6 +18,20 @@ const julday = (date: Date, gregflag: number): Promise<number> => {
   )
 }
 
+const ALL_PLANETS = [
+  'SUN',
+  'MOON',
+  'MERCURY',
+  'VENUS',
+  'MARS',
+  'JUPITER',
+  'SATURN',
+  'URANUS',
+  'NEPTUNE',
+  'PLUTO',
+] as const
+type PlanetName = typeof ALL_PLANETS[number]
+
 type EclipticPosition = {
   longitude: number
   latitude: number
@@ -28,41 +42,48 @@ type EclipticPosition = {
   rflag: number
   error?: any
 }
+type PlanetPosition = { planet: PlanetName } & EclipticPosition
 
 const eclipticPosition = (julday_ut: number, planetNumber: number, iflag: number): Promise<EclipticPosition> =>
-  new Promise((resolve) =>
-    swisseph.swe_calc_ut(julday_ut, planetNumber, iflag, (body: EclipticPosition) => resolve(body))
+  new Promise((resolve, reject) =>
+    swisseph.swe_calc_ut(julday_ut, planetNumber, iflag, (body: EclipticPosition) => {
+      if (body.error) reject(body.error)
+      resolve(body)
+    })
   )
-
-const planets = ['SUN', 'MOON', 'MERCURY', 'VENUS', 'MARS', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO'] as const
 
 // Test date
 const date = new Date('1987-09-08T08:53:00+09:00')
 console.log('Test date:', date)
 
 // Julian day
-julday(date, swisseph.SE_GREG_CAL).then((julday_ut) => {
+julday(date, swisseph.SE_GREG_CAL).then(async (julday_ut) => {
   assert.equal(julday_ut, 2447046.4951388887)
   console.log('Julian UT day for date:', julday_ut)
 
-  planets.forEach((planet) => {
-    // position
-    eclipticPosition(julday_ut, swisseph[`SE_${planet}`], swisseph.SEFLG_SPEED).then((body: EclipticPosition) => {
-      assert(!body.error, body.error)
-      const fullDegrees = body.longitude
+  const positions = await Promise.all(
+    ALL_PLANETS.map(
+      async (planet): Promise<PlanetPosition> => {
+        const position = await eclipticPosition(julday_ut, swisseph[`SE_${planet}`], swisseph.SEFLG_SPEED)
+        return { planet, ...position }
+      }
+    )
+  )
 
-      const sign = Math.trunc(fullDegrees / 30)
+  for (const position of positions) {
+    const fullDegrees = position.longitude
 
-      const degrees = fullDegrees % 30
-      const degreesInt = Math.trunc(degrees)
-      const degreesMin = (degrees - degreesInt) * 60
-      const degreesMinInt = Math.trunc(degreesMin)
-      const degreesSec = (degreesMin - degreesMinInt) * 60
-      const degreesSecInt = Math.trunc(degreesSec)
-      const degreesToPrint = `${String(degreesInt).padStart(2)}°${String(degreesMinInt).padStart(2, '0')}′${String(
-        degreesSecInt
-      ).padStart(2, '0')}″`
-      console.log(`${planet}:`, { sign, degrees: degreesToPrint })
-    })
-  })
+    const sign = Math.trunc(fullDegrees / 30)
+
+    const degrees = fullDegrees % 30
+    const degreesInt = Math.trunc(degrees)
+    const degreesMin = (degrees - degreesInt) * 60
+    const degreesMinInt = Math.trunc(degreesMin)
+    const degreesSec = (degreesMin - degreesMinInt) * 60
+    const degreesSecInt = Math.trunc(degreesSec)
+    const degreesToPrint = `${String(degreesInt).padStart(2)}°${String(degreesMinInt).padStart(2, '0')}′${String(
+      degreesSecInt
+    ).padStart(2, '0')}″`
+    console.log(`${position.planet}:`, { sign, degrees: degreesToPrint })
+  }
 })

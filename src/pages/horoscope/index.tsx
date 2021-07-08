71fc3b82a9efa496d4af0b9f150ef7ad617970a2
ikegamikanae <fujii_kanae@hotmail.com>
2021-07-09 00:01:37 +0900

    サイン区分のダミーテーブル追加

--- src/pages/horoscope/index.tsx
+++ src/pages/horoscope/index.tsx
@@ -39,6 +39,7 @@ function Horoscope() {
   if (error) return <div>failed to load: {JSON.stringify(error.message)}</div>
   if (!data) return <div>loading...</div>
 
+  console.log(data)
   const planets = Object.entries(data).map(([key, value]) => ({ name: key, longitude: value.longitude }))
 
   return (
@@ -58,6 +59,61 @@ function Horoscope() {
               <td>{planet.longitude}</td>
             </tr>
           ))}
+        </tbody>
+      </table>
+      <p>Sign</p>
+      <table>
+        <thead>
+          <tr>
+            <td>区分</td>
+            <td>惑星</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>火：</td>
+            <td>木星　天王星　海王星</td>
+          </tr>
+          <tr>
+            <td>土：</td>
+            <td>太陽　水星</td>
+          </tr>
+          <tr>
+            <td>風：</td>
+            <td>火星　土星　冥王星</td>
+          </tr>
+          <tr>
+            <td>水：</td>
+            <td>月　金星</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr>
+            <td>活動宮：</td>
+            <td>月　金星　土星　冥王星</td>
+          </tr>
+          <tr>
+            <td>不動宮：</td>
+            <td>太陽　水星</td>
+          </tr>
+          <tr>
+            <td>柔軟宮：</td>
+            <td>火星　木星　天王星　海王星</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr>
+            <td>男性宮：</td>
+            <td>火星　木星　土星　天王星　海王星　冥王星</td>
+          </tr>
+          <tr>
+            <td>女性宮：</td>
+            <td>太陽　月　水星　金星</td>
+          </tr>
         </tbody>
       </table>
     </>

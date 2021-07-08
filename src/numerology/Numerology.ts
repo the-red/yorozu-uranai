b960232f828dcf23c18810b8e19a7641e2d8a050
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-07-09 07:23:52 +0900

    refactor: ピュタゴリアン変換を切り出す

--- src/numerology/Numerology.ts
+++ src/numerology/Numerology.ts
@@ -28,49 +28,51 @@ export class Numerology {
   }
 
   // 生年月日が対象
-  lifePathNumber(nums?: number[]): number {
+  get lifePathNumber(): number {
     const digits = [...this.YYYY.split(''), ...this.MM.split(''), ...this.DD.split('')].map((v) => Number(v))
     return this.sumOfDigits(digits)
   }
 
   // ローマ字表記の名前を数字に置き換えたものが対象
-  // （ピュタゴリアン変換）。ABC...IJ... -> 123...91...
-  destinyNumber(): number {
-    const digits = [...this.fullName.toUpperCase().split('')]
-      .map((v) => v.charCodeAt(0) - 'A'.charCodeAt(0) + 1)
-      .map((v) => v % 9 || 9)
+  get destinyNumber(): number {
+    const digits = [...this.fullName.toUpperCase().split('')].map(this.putagorianConvert)
     return this.sumOfDigits(digits)
   }
 
   // ローマ字表記の名前の母音を数字に置き換えたものが対象
-  soulNumber(): number {
+  get soulNumber(): number {
     const digits = [...this.fullName.toUpperCase().split('')]
       .filter((v) => this.VOWELS.includes(v))
-      .map((v) => v.charCodeAt(0) - 'A'.charCodeAt(0) + 1)
-      .map((v) => v % 9 || 9)
+      .map(this.putagorianConvert)
     return this.sumOfDigits(digits)
   }
 
   // ローマ字表記の名前の子音を数字に置き換えたものが対象
-  personalityNumber(): number {
+  get personalityNumber(): number {
     const digits = [...this.fullName.toUpperCase().split('')]
       .filter((v) => !this.VOWELS.includes(v))
-      .map((v) => v.charCodeAt(0) - 'A'.charCodeAt(0) + 1)
-      .map((v) => v % 9 || 9)
+      .map(this.putagorianConvert)
     return this.sumOfDigits(digits)
   }
 
-  maturityNumber(): number {
-    return this.lifePathNumber() + this.destinyNumber()
+  get maturityNumber(): number {
+    return this.lifePathNumber + this.destinyNumber
   }
 
-  birthdayNumber(): number {
+  get birthdayNumber(): number {
     return this.sumOfDigits(this.DD.split('').map((v) => Number(v)))
   }
 
+  // 各桁の和を求める。ゾロ目または1桁になるまで再帰的に計算する。
   private sumOfDigits(digits: number[]): number {
     const sum = digits.reduce((prev, curr) => prev + curr, 0)
     if (sum % 11 === 0 || sum % 111 === 0 || Math.floor(sum / 10) === 0) return sum
     return this.sumOfDigits([Math.floor(sum / 10), sum % 10])
+  }
+
+  // ABC...IJ... -> 123...91... に変換する（ピュタゴリアン変換）
+  private putagorianConvert(char: string): number {
+    const temp = char.charCodeAt(0) - 'A'.charCodeAt(0) + 1
+    return temp % 9 || 9
   }
 }
--- test/numerology/Numerology.test.ts
+++ test/numerology/Numerology.test.ts
@@ -3,21 +3,21 @@ import { Numerology } from '../../src/numerology/Numerology'
 describe('Numerology', () => {
   const numerology = new Numerology({ birthDate: new Date('1970-10-31'), firstName: 'SUHI', lastName: 'KAZUYA' })
   it('life path number', () => {
-    expect(numerology.lifePathNumber()).toEqual(22)
+    expect(numerology.lifePathNumber).toEqual(22)
   })
   it('destiny number', () => {
-    expect(numerology.destinyNumber()).toEqual(7)
+    expect(numerology.destinyNumber).toEqual(7)
   })
   it('soul number', () => {
-    expect(numerology.soulNumber()).toEqual(8)
+    expect(numerology.soulNumber).toEqual(8)
   })
   it('personality number', () => {
-    expect(numerology.personalityNumber()).toEqual(8)
+    expect(numerology.personalityNumber).toEqual(8)
   })
   it('maturity number', () => {
-    expect(numerology.maturityNumber()).toEqual(29)
+    expect(numerology.maturityNumber).toEqual(29)
   })
   it('birthday number', () => {
-    expect(numerology.birthdayNumber()).toEqual(4)
+    expect(numerology.birthdayNumber).toEqual(4)
   })
 })

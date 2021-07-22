f6c71a035fb22848e05caa87a0f4ecc2d927d050
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-07-22 13:20:37 +0900

    3桁以上のゾロ目を考慮

--- test/numerology/Numerology.test.ts
+++ test/numerology/Numerology.test.ts
@@ -1,7 +1,7 @@
 import { Numerology } from '../../src/numerology/Numerology'
 
 describe('Numerology', () => {
-  const numerology = new Numerology({ birthDate: new Date('1970-10-31'), fullName: 'SUHIKAZUYA' })
+  const numerology = new Numerology({ birthDate: new Date('1970-10-31'), fullName: 'SUHI KAZUYA' })
   it('life path number', () => {
     expect(numerology.lifePathNumber).toEqual(22)
   })
@@ -19,5 +19,16 @@ describe('Numerology', () => {
   })
   it('birthday number', () => {
     expect(numerology.birthdayNumber).toEqual(4)
-  })
+  }),
+    describe('sumOfDigits', () => {
+      const one = 'A'
+      const ten = 'AI'
+      const hundred = ten.repeat(10)
+      it('2桁', () => {
+        expect(new Numerology({ birthDate: new Date(), fullName: one + ten }).destinyNumber).toEqual(11)
+      })
+      it('3桁', () => {
+        expect(new Numerology({ birthDate: new Date(), fullName: one + ten + hundred }).destinyNumber).toEqual(111)
+      })
+    })
 })

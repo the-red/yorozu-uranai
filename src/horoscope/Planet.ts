04d9b28f115670f72fd45d33a662ad00e3662882
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-07-30 16:49:55 +0900

    Revert "Form submit"

--- src/horoscope/Planet.ts
+++ src/horoscope/Planet.ts
@@ -120,14 +120,6 @@ export class Planet {
     }
   }
 
-  get coordinate() {
-    // ホロスコープに描画する際の座標（左端中央が原点）
-    const radian = (this.longitude - 90) * (Math.PI / 180)
-    const x = Math.sin(radian)
-    const y = Math.cos(radian)
-    return { x, y }
-  }
-
   diffLongitude(targetLongitude: number): number {
     return Math.abs(targetLongitude - this.longitude)
   }
@@ -152,7 +144,6 @@ export class Planet {
       element: this.element,
       quality: this.quality,
       polarity: this.polarity,
-      coordinate: this.coordinate,
     }
   }
 }

7508cb8a2e6601eb68caf9475ec7ddbe11f4d32f
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-02 11:59:17 +0900

    fix: useSWRに更新後の値が正しくわたるように

--- src/pages/horoscope/index.tsx
+++ src/pages/horoscope/index.tsx
@@ -44,28 +44,20 @@ function HoroscopePage() {
   // }
 
   const [horoscopeSeed, setHoroscopeSeed] = useState<HoroscopeSeed>(defaultValues)
-  const { data: horoscope, error } = useSWR(
-    '/api/horoscope',
-    async (url) => {
-      console.log('swr', horoscopeSeed.birthday)
-      const bd = document.querySelector('#birthday-text')?.textContent
-      console.log(bd)
-      const res = await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        // @ts-ignore
-        body: JSON.stringify({ ...horoscopeSeed, birthday: new Date(bd) }),
-      })
-      if (!res.ok) {
-        const { errorMessage } = await res.json()
-        throw new Error(errorMessage)
-      }
-      const json = await res.json()
-      const { houses, ...planets } = json.data
-      return { houses, planets } as Horoscope
-    },
-    { onSuccess: (data) => console.log('success!', data.planets.sun), refreshInterval: 1000 }
-  )
+  const { data: horoscope, error } = useSWR(['/api/horoscope', horoscopeSeed], async (url, horoscopeSeed) => {
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(horoscopeSeed),
+    })
+    if (!res.ok) {
+      const { errorMessage } = await res.json()
+      throw new Error(errorMessage)
+    }
+    const json = await res.json()
+    const { houses, ...planets } = json.data
+    return { houses, planets } as Horoscope
+  })
 
   if (error) return <div>failed to load: {JSON.stringify(error.message)}</div>
   if (!horoscope) return <div>loading...</div>
@@ -118,9 +110,6 @@ function HoroscopePage() {
           </div>
         </div>
       </div>
-      <p style={{ visibility: 'hidden' }} id="birthday-text">
-        {horoscopeSeed.birthday.toISOString()}
-      </p>
     </div>
   )
 }

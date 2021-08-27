6ba0ee0bf2ccc741c7c7a327f576809c8528cde5
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2021-08-27 13:35:19 +0900

    refactor: calcSignCoordinate関数廃止、コンポ内に閉じ込める

--- src/components/HoroscopeCircle.tsx
+++ src/components/HoroscopeCircle.tsx
@@ -22,9 +22,7 @@ type Horoscope = {
 
 const origin = 250
 const signRadius = 220
-const planetRadius = 150
-const iconSizeSign = 24
-const iconSizePlanet = 20
+const planetRadius = signRadius * 0.8
 
 const calcLineCoordinate = (longitude: number) => {
   const radian1 = (longitude - 90) * (Math.PI / 180)
@@ -36,51 +34,52 @@ const calcLineCoordinate = (longitude: number) => {
   return [x1, y1, x2, y2]
 }
 
-const calcSignCoordinate = (longitude: number) => {
-  const radian = (longitude - 75) * (Math.PI / 180)
-  const x = origin + Math.sin(radian) * signRadius * 0.9
-  const y = origin + Math.cos(radian) * signRadius * 0.9
-  return { x, y }
-}
 const signCoordinates = [
-  { name: '牡羊座', icon: '♈', coordinate: calcSignCoordinate(0), url: '/images/astro-sign-1.png' },
-  { name: '牡牛座', icon: '♉', coordinate: calcSignCoordinate(30), url: '/images/astro-sign-2.png' },
-  { name: '双子座', icon: '♊', coordinate: calcSignCoordinate(60), url: '/images/astro-sign-3.png' },
-  { name: '蟹座', icon: '♋', coordinate: calcSignCoordinate(90), url: '/images/astro-sign-4.png' },
-  { name: '獅子座', icon: '♌', coordinate: calcSignCoordinate(120), url: '/images/astro-sign-5.png' },
-  { name: '乙女座', icon: '♍', coordinate: calcSignCoordinate(150), url: '/images/astro-sign-6.png' },
-  { name: '天秤座', icon: '♎', coordinate: calcSignCoordinate(180), url: '/images/astro-sign-7.png' },
-  { name: '蠍座', icon: '♏', coordinate: calcSignCoordinate(210), url: '/images/astro-sign-8.png' },
-  { name: '射手座', icon: '♐', coordinate: calcSignCoordinate(240), url: '/images/astro-sign-9.png' },
-  { name: '山羊座', icon: '♑', coordinate: calcSignCoordinate(270), url: '/images/astro-sign-10.png' },
-  { name: '水瓶座', icon: '♒', coordinate: calcSignCoordinate(300), url: '/images/astro-sign-11.png' },
-  { name: '魚座', icon: '♓', coordinate: calcSignCoordinate(330), url: '/images/astro-sign-12.png' },
+  { name: '牡羊座', icon: '♈', longitude: 0, url: '/images/astro-sign-1.png' },
+  { name: '牡牛座', icon: '♉', longitude: 30, url: '/images/astro-sign-2.png' },
+  { name: '双子座', icon: '♊', longitude: 60, url: '/images/astro-sign-3.png' },
+  { name: '蟹座', icon: '♋', longitude: 90, url: '/images/astro-sign-4.png' },
+  { name: '獅子座', icon: '♌', longitude: 120, url: '/images/astro-sign-5.png' },
+  { name: '乙女座', icon: '♍', longitude: 150, url: '/images/astro-sign-6.png' },
+  { name: '天秤座', icon: '♎', longitude: 180, url: '/images/astro-sign-7.png' },
+  { name: '蠍座', icon: '♏', longitude: 210, url: '/images/astro-sign-8.png' },
+  { name: '射手座', icon: '♐', longitude: 240, url: '/images/astro-sign-9.png' },
+  { name: '山羊座', icon: '♑', longitude: 270, url: '/images/astro-sign-10.png' },
+  { name: '水瓶座', icon: '♒', longitude: 300, url: '/images/astro-sign-11.png' },
+  { name: '魚座', icon: '♓', longitude: 330, url: '/images/astro-sign-12.png' },
 ]
 
 const SignImage = ({ signCoordinate }: { signCoordinate: typeof signCoordinates[number] }) => {
   const [image] = useImage(signCoordinate.url)
+  const iconSize = signRadius * 0.12
+  const radian = (signCoordinate.longitude - 75) * (Math.PI / 180)
+
   return (
     <Image
       image={image}
-      x={signCoordinate.coordinate.x}
-      y={signCoordinate.coordinate.y}
-      width={iconSizeSign}
-      height={iconSizeSign}
-      offset={{ x: iconSizeSign / 2, y: iconSizeSign / 2 }}
+      x={origin + Math.sin(radian) * signRadius * 0.9}
+      y={origin + Math.cos(radian) * signRadius * 0.9}
+      width={iconSize}
+      height={iconSize}
+      offset={{ x: iconSize / 2, y: iconSize / 2 }}
     />
   )
 }
 
-const PlanetImage = ({ planet }: { planet: Planet }) => (
-  <Text
-    text={planet.icon}
-    x={origin + planet.coordinate.x * planetRadius}
-    y={origin + planet.coordinate.y * planetRadius}
-    fontSize={iconSizePlanet}
-    offset={{ x: iconSizePlanet / 2, y: iconSizePlanet / 2 }}
-    fill="black"
-  />
-)
+const PlanetImage = ({ planet }: { planet: Planet }) => {
+  const radiusScale = planetRadius * 0.85
+  const iconSize = planetRadius * 0.12
+  return (
+    <Text
+      text={planet.icon}
+      x={origin + planet.coordinate.x * radiusScale}
+      y={origin + planet.coordinate.y * radiusScale}
+      fontSize={iconSize}
+      offset={{ x: iconSize / 2, y: iconSize / 2 }}
+      fill="black"
+    />
+  )
+}
 
 export default function HoroscopeCircle({ horoscope }: { horoscope: Horoscope }) {
   const { planets } = horoscope
@@ -91,8 +90,8 @@ export default function HoroscopeCircle({ horoscope }: { horoscope: Horoscope })
         {/* 四角 */}
         {/* <Rect fill="gray" x={100} y={100} width={300} height={200} /> */}
         {/* 円 */}
-        <Circle stroke="#352e2b" strokeWidth={1} fill="#e4E7E2" x={origin} y={origin} radius={220} opacity={1} />
-        <Circle stroke="#352e2b" strokeWidth={1} fill="white" x={origin} y={origin} radius={180} opacity={1} />
+        <Circle stroke="#352e2b" strokeWidth={1} fill="#e4E7E2" x={origin} y={origin} radius={signRadius} opacity={1} />
+        <Circle stroke="#352e2b" strokeWidth={1} fill="white" x={origin} y={origin} radius={planetRadius} opacity={1} />
         <Circle stroke="#afb1b1" strokeWidth={1} fill="#e4E7E2" x={origin} y={origin} radius={100} opacity={1} />
         {/* 線 */}
         <Line points={calcLineCoordinate(0)} stroke="black" strokeWidth={1} opacity={0.2} />

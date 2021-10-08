7e98a485d392b2142b7c85eb70685b3ddc7582b9
funady <funada@sonicgarden.jp>
2021-10-08 14:43:12 +0900

    AspectChartでreturnする中身を記述

--- src/components/AspectChart.tsx
+++ src/components/AspectChart.tsx
@@ -1,3 +1,84 @@
 export default function AspectChart() {
-  return <div style={{ fontSize: '30px' }}>Aspect Chart</div>
+  return (
+    <div>
+      <div style={{ fontSize: '30px' }}>Aspect Chart</div>
+      <div className="container">
+        <div className="outer-item">
+          <div className="inner-item planet-icon">⦿</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">60</div>
+          <div className="inner-item planet-icon">☽</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">120</div>
+          <div className="inner-item">120</div>
+          <div className="inner-item planet-icon">☿</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">0</div>
+          <div className="inner-item">0</div>
+          <div className="inner-item">0</div>
+          <div className="inner-item planet-icon">♀</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item planet-icon">♂</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">180</div>
+          <div className="inner-item">180</div>
+          <div className="inner-item">180</div>
+          <div className="inner-item">180</div>
+          <div className="inner-item">180</div>
+          <div className="inner-item planet-icon">♃</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item planet-icon">♄</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item planet-icon">♅</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item planet-icon">♆</div>
+        </div>
+        <div className="outer-item">
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item">90</div>
+          <div className="inner-item planet-icon">♇</div>
+        </div>
+      </div>
+    </div>
+  )
 }

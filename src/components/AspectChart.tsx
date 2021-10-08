cf813756ff40dbefe222232fbd3973be014594c2
funady <funada@sonicgarden.jp>
2021-10-08 16:04:46 +0900

    CSSをあてた

--- src/components/AspectChart.tsx
+++ src/components/AspectChart.tsx
@@ -1,82 +1,84 @@
+import styles from '../pages/horoscope/HoroscopePage.module.css'
+
 export default function AspectChart() {
   return (
     <div>
       <div style={{ fontSize: '30px' }}>Aspect Chart</div>
-      <div className="container">
-        <div className="outer-item">
-          <div className="inner-item planet-icon">⦿</div>
+      <div className={styles['aspect-chart-container']}>
+        <div className={styles['outer-item']}>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>⦿</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">60</div>
-          <div className="inner-item planet-icon">☽</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>60</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>☽</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">120</div>
-          <div className="inner-item">120</div>
-          <div className="inner-item planet-icon">☿</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>120</div>
+          <div className={styles['inner-item']}>120</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>☿</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">0</div>
-          <div className="inner-item">0</div>
-          <div className="inner-item">0</div>
-          <div className="inner-item planet-icon">♀</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>0</div>
+          <div className={styles['inner-item']}>0</div>
+          <div className={styles['inner-item']}>0</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♀</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item planet-icon">♂</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♂</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">180</div>
-          <div className="inner-item">180</div>
-          <div className="inner-item">180</div>
-          <div className="inner-item">180</div>
-          <div className="inner-item">180</div>
-          <div className="inner-item planet-icon">♃</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>180</div>
+          <div className={styles['inner-item']}>180</div>
+          <div className={styles['inner-item']}>180</div>
+          <div className={styles['inner-item']}>180</div>
+          <div className={styles['inner-item']}>180</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♃</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item planet-icon">♄</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♄</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item planet-icon">♅</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♅</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item planet-icon">♆</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♆</div>
         </div>
-        <div className="outer-item">
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item">90</div>
-          <div className="inner-item planet-icon">♇</div>
+        <div className={styles['outer-item']}>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={styles['inner-item']}>90</div>
+          <div className={`${styles['inner-item']} ${styles['planet-icon']}`}>♇</div>
         </div>
       </div>
     </div>

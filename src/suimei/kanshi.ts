5126119f7fa52e9229a5136b79327cc2b6ea6efb
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-02-24 19:40:39 +0900

    refactor: 六十干支は十干x十二支で計算

--- src/suimei/kanshi.ts
+++ src/suimei/kanshi.ts
@@ -1,68 +1,22 @@
 import { DateTime } from 'luxon'
 
+const 十干 = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'] as const
+const 十二支 = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'] as const
+type T干支 = `${typeof 十干[number]}${typeof 十二支[number]}`
+
+const get六十干支 = (): T干支[] => {
+  const 六十干支: T干支[] = []
+
+  for (let i = 0; i < 60; i++) {
+    const 干 = 十干[i % 10]
+    const 支 = 十二支[i % 12]
+    六十干支.push(`${干}${支}`)
+  }
+  return 六十干支
+}
+
 export class Kanshi {
-  static readonly 干支 = [
-    '甲子',
-    '乙丑',
-    '丙寅',
-    '丁卯',
-    '戊辰',
-    '己巳',
-    '庚午',
-    '辛未',
-    '壬申',
-    '癸酉',
-    '甲戌',
-    '乙亥',
-    '丙子',
-    '丁丑',
-    '戊寅',
-    '己卯',
-    '庚辰',
-    '辛巳',
-    '壬午',
-    '癸未',
-    '甲申',
-    '乙酉',
-    '丙戌',
-    '丁亥',
-    '戊子',
-    '己丑',
-    '庚寅',
-    '辛卯',
-    '壬辰',
-    '癸巳',
-    '甲午',
-    '乙未',
-    '丙申',
-    '丁酉',
-    '戊戌',
-    '己亥',
-    '庚子',
-    '辛丑',
-    '壬寅',
-    '癸卯',
-    '甲辰',
-    '乙巳',
-    '丙午',
-    '丁未',
-    '戊申',
-    '己酉',
-    '庚戌',
-    '辛亥',
-    '壬子',
-    '癸丑',
-    '甲寅',
-    '乙卯',
-    '丙辰',
-    '丁巳',
-    '戊午',
-    '己未',
-    '庚申',
-    '辛酉',
-    '壬戌',
-    '癸亥',
-  ] as const
+  static readonly 六十干支 = get六十干支()
 
   // 月柱・日柱が甲子(index:0)となる基準日
   // TODO: 年柱もセットで3つが甲子になる日を見つけたい
@@ -70,36 +24,34 @@ export class Kanshi {
 
   constructor(private readonly date: DateTime) {}
 
-  get 年柱(): typeof Kanshi.干支[number] {
+  get 年柱(): T干支 {
     const year = this.date.year
 
     // TODO: 立春を考慮して調整する
     // 今は立春後のindexで固定しているので、立春前（1月と2月頭）は結果が違う可能性あり
     const index = (year % 60) - 4
 
-    // @ts-expect-error
-    return Kanshi.干支.at(index)
+    return Kanshi.六十干支.at(index)!
   }
 
-  get 月柱(): typeof Kanshi.干支[number] {
+  get 月柱(): T干支 {
     const diff = this.date.diff(Kanshi.BASE, 'months').months
 
     // TODO: 節入日を考慮して調整する
     // 今は節入後のindexで固定しているので、節入前（月の前半）は結果が違う可能性あり
     const index = (diff % 60) + 1
 
-    // @ts-expect-error
-    return Kanshi.干支.at(index)
+    return Kanshi.六十干支.at(index)!
   }
 
-  get 日柱(): typeof Kanshi.干支[number] {
+  get 日柱(): T干支 {
     const diff = this.date.diff(Kanshi.BASE, 'days').days
     const index = diff % 60
-    // @ts-expect-error
-    return Kanshi.干支.at(index)
+
+    return Kanshi.六十干支.at(index)!
   }
 
-  get 時柱(): typeof Kanshi.干支[number] {
+  get 時柱(): T干支 {
     // TODO: 位置情報を考慮して時刻を微調整する
     // 四柱推命の本P22を参照
     // ホロスコープも同じ調整を入れるべきか？
@@ -107,8 +59,7 @@ export class Kanshi {
 
     const index = Math.trunc(hour / 2) + this.日干index * 12
 
-    // @ts-expect-error
-    return Kanshi.干支.at(index)
+    return Kanshi.六十干支.at(index)!
   }
 
   private get 日干index(): number {

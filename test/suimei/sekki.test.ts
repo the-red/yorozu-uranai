b3c6b04d47867782a2ed757af59bf44a423839d1
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-02-25 00:40:39 +0900

    test: 日時→節気の変換テスト

--- test/suimei/sekki.test.ts
+++ test/suimei/sekki.test.ts
@@ -1,4 +1,5 @@
 import { sekkiIndex, sekki } from '../../src/suimei/sekki'
+import { getLongitude } from '../../src/horoscope/swisseph'
 
 describe('二十四節気', () => {
   it('315', () => {
@@ -15,5 +16,21 @@ describe('二十四節気', () => {
   it('314.99', () => {
     expect(sekkiIndex(314.99)).toEqual(11)
     expect(sekki(314.99)).toEqual('小寒')
+  })
+})
+
+describe('日時→節気 変換', () => {
+  it('小寒の最終日時分秒', async () => {
+    expect(sekki(await getLongitude(new Date('2022-02-04T05:50:45+09:00')))).toEqual('小寒')
+  })
+  it('立春の開始日時分秒', async () => {
+    expect(sekki(await getLongitude(new Date('2022-02-04T05:50:46+09:00')))).toEqual('立春')
+  })
+
+  it('立冬の最終日時分秒', async () => {
+    expect(sekki(await getLongitude(new Date('2018-12-07T13:25:54+09:00')))).toEqual('立冬')
+  })
+  it('大雪の開始日時分秒', async () => {
+    expect(sekki(await getLongitude(new Date('2018-12-07T13:25:55+09:00')))).toEqual('大雪')
   })
 })

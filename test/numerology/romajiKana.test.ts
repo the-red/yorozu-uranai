ab500823bdcfadf509d2f52a4fa0c45cfa6fcb8f
ikegamikanae <fujii_kanae@hotmail.com>
2022-03-24 13:21:04 +0900

    ライブラリを使うよう修正

--- test/numerology/romajiKana.test.ts
+++ test/numerology/romajiKana.test.ts
@@ -3,70 +3,57 @@ import { convertHiraganaToRomagi } from '../../src/numerology/romajiKana'
 describe('convertHiraganaToRomagi', () => {
   describe('五十音と撥音（ん）', () => {
     it('３文字', () => {
-      expect(convertHiraganaToRomagi('こよみ')).toEqual('koyomi')
+      const hiraganaString = 'こよみ'
+      const romajiString = 'KOYOMI'
+      expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('長い文字', () => {
       const hiraganaString = 'にわにはにわのにわとり'
-      const romajiString = 'niwanihaniwanoniwatori'
+      const romajiString = 'niwanihaniwanoniwatori'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('わをん', () => {
       const hiraganaString = 'わをん'
-      const romajiString = 'wawon'
+      const romajiString = 'wawon'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('「ち」"chi"、「つ」"tsu"', () => {
       const hiraganaString = 'たちつてと'
-      const romajiString = 'tachitsuteto'
+      const romajiString = 'tachitsuteto'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('はひふへほ', () => {
       const hiraganaString = 'はひふへほ'
-      const romajiString = 'hahihuheho'
+      const romajiString = 'hahifuheho'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
   })
   describe('拗音、促音', () => {
-    it('ゃゅょ', () => {
-      const hiraganaString = 'ゃゅょ'
-      const romajiString = 'lyalyulyo'
-      expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
-    })
-    it('っ', () => {
-      const hiraganaString = 'っ'
-      const romajiString = 'ltu'
-      expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
-    })
-    it('ぁぃぅぇぉ', () => {
-      const hiraganaString = 'ぁぃぅぇぉ'
-      const romajiString = 'lalilulelo'
-      expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
-    })
     it('文章中の拗音、促音', () => {
       const hiraganaString = 'とうきょうとっきょきょかきょく'
-      const romajiString = 'toukyoutokkyokyokakyoku'
+      const romajiString = 'toukyoutokkyokyokakyoku'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
   })
   describe('濁点付き', () => {
     it('「ば」「が」', () => {
       const hiraganaString = 'ばすがすばくはつ'
-      const romajiString = 'basugasubakuhatu'
+      const romajiString = 'basugasubakuhatsu'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('ばびぶべぼ', () => {
       const hiraganaString = 'ばびぶべぼ'
-      const romajiString = 'babibubebo'
+      const romajiString = 'babibubebo'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('がぎぐげご', () => {
       const hiraganaString = 'がぎぐげご'
-      const romajiString = 'gagigugego'
+      const romajiString = 'gagigugego'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('だぢづでど', () => {
       const hiraganaString = 'だぢづでど'
-      const romajiString = 'dadidudedo'
+      const romajiString = 'dajizudedo'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
   })
@@ -74,12 +61,12 @@ describe('convertHiraganaToRomagi', () => {
   describe('半濁点つき', () => {
     it('ぱぴぷぺぽ', () => {
       const hiraganaString = 'ぱぴぷぺぽ'
-      const romajiString = 'papipupepo'
+      const romajiString = 'papipupepo'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
     it('「ポッポ」', () => {
       const hiraganaString = 'ぽっぽ'
-      const romajiString = 'poltupo'
+      const romajiString = 'poppo'.toUpperCase()
       expect(convertHiraganaToRomagi(hiraganaString)).toEqual(romajiString)
     })
   })

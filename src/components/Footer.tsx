ad6fee5c634ec9dc7b69655c6ea390555e33ed1f
funayu <funayu0908@gmail.com>
2022-04-20 16:28:28 +0900

    フッターにtwitterリンクを追加

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,18 @@
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import twitterLogo from '../../public/images/index/twitter-logo.svg'
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
+      <p className={styles.contact}>
+        <a href="https://twitter.com/YorozuUranai" target="_blank">
+          <Image src={twitterLogo} width={25} height={25} alt="Twitterアイコン" />
+        </a>
+      </p>
       <p className={styles.copyright}>
         <small>
-          <a href="https://www.sonicgarden.jp" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.sonicgarden.jp" target="_blank" rel="noopener noreferrecr">
             Copyright © SonicGarden All rights reserved.
           </a>
         </small>

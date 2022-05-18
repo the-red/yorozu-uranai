54ae6eb63beb0fba0af28e631118138a6f3e63ec
funayu <funayu0908@gmail.com>
2022-05-18 11:16:32 +0900

    スマホ版中央寄せ調整、背景をリピート設定

--- src/components/HoroscopeDetailPage.tsx
+++ src/components/HoroscopeDetailPage.tsx
@@ -84,8 +84,11 @@ function HoroscopeDetailPage() {
             />
           </div>
         </div>
-        <div className={`${styles.content} ${styles.circle}`}>
+        <div className={`${styles.content} ${styles.circle} ${styles.pc}`}>
           <HoroscopeCircle horoscope={horoscope} origin={{ x: 250, y: 250 }} radius={220} orb={orb} />
+        </div>
+        <div className={`${styles.content} ${styles.circle} ${styles.sp}`}>
+          <HoroscopeCircle horoscope={horoscope} origin={{ x: 180, y: 250 }} radius={170} orb={orb} />
         </div>
       </div>
       <div className={styles['content-row']}>

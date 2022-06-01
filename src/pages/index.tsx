dd7b8c7219e8e8f056c0c2b56942af6105786806
funayu <funayu0908@gmail.com>
2022-06-01 11:46:43 +0900

    ファビコンファイルの削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,8 @@ export default function Home() {
       <Head>
         <title>よろず占い</title>
         <meta name="description" content="Fortune Telling" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* TODO:アイコンは別途設定する */}
+        {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
       <header className={styles.header}>

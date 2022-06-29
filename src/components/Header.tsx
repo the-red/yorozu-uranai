029f19beaa80963203751f109f69a915e948fc54
funayu <funayu0908@gmail.com>
2022-06-29 13:49:24 +0900

    レイアウト調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,24 +5,23 @@ import styles from '../styles/Home.module.css'
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>よろず占い</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href={pagesPath.horoscope.$url()}>
-              <a>西洋占星術</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={pagesPath.numerology.$url()}>
-              <a>数秘術</a>
-            </Link>
-          </li>
-          <li>
-            <a href="#">四柱推命</a>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.header_wrapper}>
+        <h1>よろず占い</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href={pagesPath.horoscope.$url()}>
+                <a>西洋占星術</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={pagesPath.numerology.$url()}>
+                <a>数秘術</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }

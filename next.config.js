9106d50b2f2deec825535b726e9b7ec16f1294a0
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-10 01:06:14 +0900

    chore: Install @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
+/** @type {import('next').NextConfig} */
 module.exports = {
+  reactStrictMode: true,
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin())
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
     return config
   },
 }

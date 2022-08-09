de5c6e5c1253448035ba3452862ca7061ab2d38c
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-10 01:45:33 +0900

    refactor: FooterのCSSはすべて普通のCSSに変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,14 @@
-import styles from '../styles/Home.module.css'
 import TwitterLogo from '../../public/images/index/twitter-logo.svg'
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>
-      <p className={styles.contact}>
+    <footer className="footer">
+      <p className="contact">
         <a href="https://twitter.com/YorozuUranai" target="_blank">
           <TwitterLogo width={25} height={25} alt="Twitterアイコン" />
         </a>
       </p>
-      <p className={styles.copyright}>
+      <p className="copyright">
         <small>
           <a href="https://www.sonicgarden.jp" target="_blank" rel="noopener noreferrer">
             Copyright © SonicGarden All rights reserved.

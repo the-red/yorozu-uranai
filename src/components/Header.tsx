16d3598f708e42c941632ab8ddcc709e2b2fb426
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-10 01:38:39 +0900

    refactor: HeaderのCSSはすべて普通のCSSに変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { pagesPath } from '../lib/$path'
-import styles from '../styles/Home.module.css'
 // @ts-expect-error
 import horoscopeIcon from '../../public/images/index/horoscope.svg?url'
 import NumerologyIcon from '../../public/images/index/numerology.svg'
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.header_wrapper}>
+    <header className="header">
+      <div className="header_wrapper">
         <h1>
           <Link href={pagesPath.$url()}>
             <a>よろず占い</a>

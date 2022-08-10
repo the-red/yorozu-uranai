b30de77d0685a949a96bb6d1cf60c241c193fe3f
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2022-08-10 11:25:22 +0900

    refactor: 数秘術 基底クラス専用のdivを追加

--- src/pages/numerology/index.tsx
+++ src/pages/numerology/index.tsx
@@ -179,20 +179,22 @@ const NumerologyPage: NextPage = () => {
   }
 
   return (
-    <div
-      className="numerology tw-min-h-screen tw-pb-8 tw-flex tw-flex-col tw-justify-between tw-items-center tw-space-y-4"
-      style={{ backgroundColor: '#EBEBC1', fontFamily: 'Lato Regular, Noto Sans JP Regular' }}
-    >
-      <Header />
-      <div className="tw-w-full tw-max-w-screen-md tw-px-4 tw-mx-auto tw-space-y-8">
-        <div style={{ fontFamily: 'MTF Wildflower' }} className="tw-text-center tw-text-7xl sm:tw-text-10xl">
-          numerology
-        </div>
+    <div className="numerology">
+      <div
+        className="tw-min-h-screen tw-pb-8 tw-flex tw-flex-col tw-justify-between tw-items-center tw-space-y-4"
+        style={{ backgroundColor: '#EBEBC1', fontFamily: 'Lato Regular, Noto Sans JP Regular' }}
+      >
+        <Header />
+        <div className="tw-w-full tw-max-w-screen-md tw-px-4 tw-mx-auto tw-space-y-8">
+          <div style={{ fontFamily: 'MTF Wildflower' }} className="tw-text-center tw-text-7xl sm:tw-text-10xl">
+            numerology
+          </div>
 
-        <NumerologyForm onSubmit={handleSubmit} />
-        {numerology && <CoreNumbers numerology={numerology} />}
+          <NumerologyForm onSubmit={handleSubmit} />
+          {numerology && <CoreNumbers numerology={numerology} />}
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }

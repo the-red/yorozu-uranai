bac4c8718ccd2819e98295f2958f3edb80b8b896
rtokkie <natto.and.eggs@gmail.com>
2022-09-21 12:37:55 +0900

    トップページのフォームのマークアップだけ完了

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
+import { FormEventHandler, ReactNode } from 'react'
 import { pagesPath, staticPath } from '../lib/$path'
 import { Query } from '../lib/params'
 import SuimeiIcon from '../../public/images/index/suimei.svg'
@@ -23,6 +24,13 @@ export default function Home() {
 
       <main className="main">
         <div className="menu-outer-container">
+          {/* TODO: スマホ対応する */}
+          <div className="tw-hidden sm:tw-block">
+            <div className="tw-flex tw-justify-center tw-mb-20">
+              <TopForm />
+            </div>
+          </div>
+
           <div className="menu-container">
             <div className="menu menu-horoscope">
               <Link href={pagesPath.horoscope.$url()}>
@@ -62,5 +70,95 @@ export default function Home() {
       <div className="footer-spacer"></div>
       <Footer />
     </div>
+  )
+}
+
+type TopFormProps = {}
+
+const TopForm = (props: TopFormProps) => {
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault()
+  }
+
+  const Label = ({ children }: { children: ReactNode }) => {
+    return <label className="tw-space-y-1 tw-w-280px">{children}</label>
+  }
+
+  const WrappedHoroscopeIcon = () => (
+    <Image src={staticPath.images.index.horoscope_svg} width="16" height="16" alt="西洋占星術" />
+  )
+
+  const WrappedNumerologyIcon = () => <NumerologyIcon width="16" height="16" alt="数秘術" />
+
+  const WrappedSuimeiIcon = () => <SuimeiIcon width="18" height="18" alt="四柱推命" />
+
+  return (
+    <form className="tw-space-y-10 tw-w-680px" onSubmit={handleSubmit}>
+      <div className="tw-flex">
+        <Label>
+          <div>生年月日</div>
+          <div className="tw-space-x-1">
+            <WrappedHoroscopeIcon />
+            <WrappedNumerologyIcon />
+            <WrappedSuimeiIcon />
+          </div>
+        </Label>
+
+        <div className="tw-space-y-4">
+          <input type="date" />
+          <div className="tw-flex tw-space-x-2">
+            <input type="number" min="0" max="23" className="tw-w-50px" />
+            <div>時</div>
+            <input type="number" min="0" max="59" className="tw-w-50px" />
+            <div>分</div>
+            <div>(JST)</div>
+          </div>
+          <div className="tw-flex tw-space-x-2">
+            <input id="time_unknown" type="checkbox" />
+            <label htmlFor="time_unknown">時刻不明</label>
+          </div>
+        </div>
+      </div>
+      <hr />
+
+      <div className="tw-flex">
+        <Label>
+          <div>出生場所</div>
+          <div className="tw-space-x-1">
+            <WrappedHoroscopeIcon />
+            <WrappedSuimeiIcon />
+          </div>
+        </Label>
+
+        <div className="tw-self-start tw-flex tw-space-x-4">
+          <div className="tw-flex tw-space-x-2">
+            <div>緯度</div>
+            <input type="number" min="-90" max="90" step="0.0001" className="tw-w-120px" />
+          </div>
+
+          <div className="tw-flex tw-space-x-2">
+            <div>経度</div>
+            <input type="number" min="-180" max="180" step="0.0001" className="tw-w-120px" />
+          </div>
+        </div>
+      </div>
+      <hr />
+
+      <div className="tw-flex">
+        <Label>
+          <div>名前</div>
+          <div>
+            <WrappedNumerologyIcon />
+          </div>
+        </Label>
+
+        <div>
+          <input type="text" />
+          <div className="tw-text-gray-500 tw-text-sm">*ひらがなかローマ字で入力してください</div>
+          <div>funadayu</div>
+        </div>
+      </div>
+      <hr />
+    </form>
   )
 }

f893f6f28d054dc82827c0329815d2cea42ee485
funayu <funayu0908@gmail.com>
2023-01-10 22:59:54 +0900

    フォームのスタイルを設定

--- src/suimei/components/SuimeiContent.tsx
+++ src/suimei/components/SuimeiContent.tsx
@@ -65,46 +65,56 @@ export const SuimeiContent: VFC<any> = ({ onSubmit, defaultValues }) => {
           <form action="" method="post" id="">
             <dl>
               <dt>生年月日</dt>
-              <dd>
+              <dd className="date_time">
                 <input type="date" name="date" />
                 <input type="time" name="time" />
               </dd>
               <dt>出生場所</dt>
-              <dd>
+              <dd className="longitude">
                 <label>
-                  緯度
-                  <input type="text" name="longitude" />
+                  <span>緯度</span>
+                  <input type="text" name="longitude" value="35.604839" />
                 </label>
               </dd>
-              <dd>
+              <dd className="latitude">
                 <label>
-                  経度
-                  <input type="text" name="latitude" />
+                  <span>経度</span>
+                  <input type="text" name="latitude" value="139.667717" />
                 </label>
               </dd>
               <dt>性別</dt>
-              <dd>
+              <dd className="gender">
                 <ul>
+                  {/* <li>
+                    <label>
+                      <input type="radio" name="gender" value="未選択" checked />
+                      <div>未選択</div>
+                    </label>
+                  </li> */}
                   <li>
                     <label>
                       <input type="radio" name="gender" value="未選択" checked />
-                      未選択
+                      <div>未選択</div>
                     </label>
                   </li>
                   <li>
                     <label>
-                      <input type="radio" name="gender" value="男" />男
+                      <input type="radio" name="gender" value="男性" />
+                      <div>男性</div>
                     </label>
                   </li>
                   <li>
                     <label>
-                      <input type="radio" name="gender" value="女" />女
+                      <input type="radio" name="gender" value="女性" />
+                      <div>女性</div>
                     </label>
                   </li>
                 </ul>
               </dd>
             </dl>
-            <button type="submit">命式を作成する</button>
+            <div className="create_meisiki_button">
+              <button type="submit">命式を作成する</button>
+            </div>
           </form>
         </div>
       </section>

cf65897430f402b5417862fb84613679ee597100
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-02-03 14:30:15 +0900

    feat: 切り捨てではなく四捨五入に

--- src/lib/math.ts
+++ src/lib/math.ts
@@ -1,2 +1,2 @@
-const rounddown = (value: number, scale: number) => Math.trunc(value * 10 ** scale) / 10 ** scale
-export const roundLatLng = (value: number) => rounddown(value, 7)
+const round = (value: number, scale: number) => Math.round(value * 10 ** scale) / 10 ** scale
+export const roundLatLng = (value: number) => round(value, 7)

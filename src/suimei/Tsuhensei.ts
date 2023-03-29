667db717dcd37a8c95b0f75e02c69f2f62602726
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-03-29 12:49:44 +0900

    refactor: 通変星 変数名を変更

--- src/suimei/Tsuhensei.ts
+++ src/suimei/Tsuhensei.ts
@@ -4,11 +4,11 @@ import type { 十干 as Jikkan } from './Kanshi'
 const tsuhenseiEven = ['比肩', '印綬', '偏印', '正官', '偏官', '正財', '偏財', '傷官', '食神', '劫財'] as const
 const tsuhenseiOdd = ['比肩', '劫財', '偏印', '印綬', '偏官', '正官', '偏財', '正財', '食神', '傷官'] as const
 
-export const tsuhensei = (nikkan: Jikkan, jikkan: Jikkan) => {
+export const tsuhensei = (nikkan: Jikkan, anotherTenkan: Jikkan) => {
   // 日干の判定：奇数か偶数か
   const nikkanIndex = jikkanList.indexOf(nikkan)
-  const jikkanIndex = jikkanList.indexOf(jikkan)
-  const tsuhenseiIndex = nikkanIndex - jikkanIndex
+  const anotherTenkanIndex = jikkanList.indexOf(anotherTenkan)
+  const tsuhenseiIndex = nikkanIndex - anotherTenkanIndex
 
   if (isEven(nikkanIndex)) {
     // 日干が偶数

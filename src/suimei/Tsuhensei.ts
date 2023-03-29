bced7696099120ad6de421f55567f0b7d76da3f0
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-03-29 13:01:01 +0900

    refactor: 関数名変更

--- src/suimei/Tsuhensei.ts
+++ src/suimei/Tsuhensei.ts
@@ -4,7 +4,7 @@ import type { 十干 as Jikkan } from './Kanshi'
 const tsuhenseiEven = ['比肩', '印綬', '偏印', '正官', '偏官', '正財', '偏財', '傷官', '食神', '劫財'] as const
 const tsuhenseiOdd = ['比肩', '劫財', '偏印', '印綬', '偏官', '正官', '偏財', '正財', '食神', '傷官'] as const
 
-export const tsuhensei = (nikkan: Jikkan, anotherTenkan: Jikkan) => {
+export const calcTsuhensei = (nikkan: Jikkan, anotherTenkan: Jikkan) => {
   // 日干の判定：奇数か偶数か
   const nikkanIndex = jikkanList.indexOf(nikkan)
   const anotherTenkanIndex = jikkanList.indexOf(anotherTenkan)

c5324de95bcb7c1e25509ead69d05155daefa278
ikegamikanae <fujii_kanae@hotmail.com>
2023-04-14 16:20:37 +0900

    feat:十二運算出の関数とテスト

--- src/suimei/Juuniun.ts
+++ src/suimei/Juuniun.ts
@@ -1,7 +1,7 @@
 import { 十干list as jikkanList, 十二支list as junishiList } from './Kanshi'
 
 const juuniunEven = ['胎', '養', '長生', '沐浴', '冠帯', '建禄', '帝旺', '衰', '病', '死', '墓', '絶']
-const juuniunOdd = juuniunEven.reverse()
+const juuniunOdd = ['絶', '墓', '死', '病', '衰', '帝旺', '建禄', '冠帯', '沐浴', '長生', '養', '胎']
 
 const startIndex = (jikkan: typeof jikkanList[number]) => {
   if (jikkan === '甲' || jikkan === '乙') {
@@ -12,5 +12,17 @@ const startIndex = (jikkan: typeof jikkanList[number]) => {
     return 3
   } else if (jikkan === '壬' || jikkan === '癸') {
     return 6
+  }
+}
+
+export const juuniun = (nikkan: typeof jikkanList[number], junishi: typeof junishiList[number]) => {
+  const nikkanIndex = startIndex(nikkan)
+  const junishiIndex = junishiList.indexOf(junishi)
+  const juuniunIndex = junishiIndex - Number(nikkanIndex)
+
+  if (jikkanList.indexOf(nikkan) % 2 === 0) {
+    return juuniunEven.at(juuniunIndex)
+  } else {
+    return juuniunOdd.at(juuniunIndex)
   }
 }

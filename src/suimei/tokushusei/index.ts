be7b7c6b8b91d5f99dfce88616761ac5c7b8a732
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-05-22 20:48:26 +0900

    feat: 日干と日支(日刃)を追加

--- src/suimei/tokushusei/index.ts
+++ src/suimei/tokushusei/index.ts
@@ -1,11 +1,17 @@
 import { Kanshi } from '../Kanshi'
 import 日干と支 from './nikkan-shi'
+import 日干と日支 from './nikkan-nisshi'
 
 export const tokushusei = (kanshi: Kanshi) => {
   const tokushuseiList: string[] = []
 
   Object.entries(日干と支).forEach(([name, rule]) => {
     if (kanshi.地支.some((shi) => rule[kanshi.日干].includes(shi))) {
+      tokushuseiList.push(name)
+    }
+  })
+  Object.entries(日干と日支).forEach(([name, rule]) => {
+    if (rule[kanshi.日干].includes(kanshi.日支)) {
       tokushuseiList.push(name)
     }
   })
--- test/suimei/Tokushusei.test.ts
+++ test/suimei/Tokushusei.test.ts
@@ -2,11 +2,10 @@ import { getKanshiInstance } from './test-util'
 import { tokushusei } from '../../src/suimei'
 
 describe('Tokushusei', () => {
-  const date = new Date('2023-03-15T11:41:20+09:00')
-
-  it('特殊星', async () => {
+  it('日干と支、日干と日支', async () => {
+    const date = new Date('2023-03-01T12:00:00+09:00')
     const kanshi = await getKanshiInstance(date)
     const tokushuseiList = tokushusei(kanshi)
-    expect(tokushuseiList).toMatchObject(['飛刃'])
+    expect(tokushuseiList).toMatchObject(['羊刃', '日刃'])
   })
 })

36355275d6725fb7b8d8c7e7b9e7f676e5b37058
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-05-22 23:56:27 +0900

    feat: 年月日時で分けて特殊星を管理

--- src/suimei/tokushusei/index.ts
+++ src/suimei/tokushusei/index.ts
@@ -1,45 +1,62 @@
 import { Kanshi } from '../Kanshi'
 import 日干と支 from './nikkan-shi'
-import 日干と日支 from './nikkan-nisshi'
 import 月支と干支 from './gesshi-kanshi'
 import 月支と支 from './gesshi-shi'
 import 年支日支と支 from './nenshi_nisshi-shi'
 import 日柱 from './nicchuu'
 
 export const tokushusei = (kanshi: Kanshi) => {
-  const tokushuseiList: string[] = []
+  const tokushuseiMap: {
+    年柱: string[]
+    月柱: string[]
+    日柱: string[]
+    時柱: string[]
+  } = {
+    年柱: [],
+    月柱: [],
+    日柱: [],
+    時柱: [],
+  }
+
+  const types = ['年', '月', '日', '時'] as const
 
   Object.entries(日干と支).forEach(([name, rule]) => {
-    if (kanshi.地支.some((shi) => rule[kanshi.日干].includes(shi))) {
-      tokushuseiList.push(name)
-    }
-  })
-  Object.entries(日干と日支).forEach(([name, rule]) => {
-    if (rule[kanshi.日干].includes(kanshi.日支)) {
-      tokushuseiList.push(name)
-    }
+    types.forEach((type) => {
+      const list = rule[kanshi.日干]
+      if (list.includes(kanshi[`${type}支`])) {
+        tokushuseiMap[`${type}柱`].push(name)
+      }
+    })
   })
   Object.entries(月支と干支).forEach(([name, rule]) => {
-    const tenkanChishi = [...kanshi.天干, ...kanshi.地支]
-    if (tenkanChishi.some((shi) => rule[kanshi.月支].includes(shi))) {
-      tokushuseiList.push(name)
-    }
+    types.forEach((type) => {
+      const list = rule[kanshi.月支]
+      if (list.includes(kanshi[`${type}干`]) || list.includes(kanshi[`${type}支`])) {
+        tokushuseiMap[`${type}柱`].push(name)
+      }
+    })
   })
   Object.entries(月支と支).forEach(([name, rule]) => {
-    if (kanshi.地支.some((shi) => rule[kanshi.月支].includes(shi))) {
-      tokushuseiList.push(name)
-    }
+    types.forEach((type) => {
+      const list = rule[kanshi.月支]
+      if (list.includes(kanshi[`${type}支`])) {
+        tokushuseiMap[`${type}柱`].push(name)
+      }
+    })
   })
   Object.entries(年支日支と支).forEach(([name, rule]) => {
-    if (kanshi.地支.some((shi) => rule[kanshi.年支].includes(shi) || rule[kanshi.日支].includes(shi))) {
-      tokushuseiList.push(name)
-    }
+    types.forEach((type) => {
+      const list = [...rule[kanshi.年支], ...rule[kanshi.日支]]
+      if (list.includes(kanshi[`${type}支`])) {
+        tokushuseiMap[`${type}柱`].push(name)
+      }
+    })
   })
-  Object.entries(日柱).forEach(([name, rule]) => {
-    if (rule.includes(kanshi.日柱)) {
-      tokushuseiList.push(name)
+  Object.entries(日柱).forEach(([name, list]) => {
+    if (list.includes(kanshi.日柱)) {
+      tokushuseiMap.日柱.push(name)
     }
   })
 
-  return tokushuseiList
+  return tokushuseiMap
 }

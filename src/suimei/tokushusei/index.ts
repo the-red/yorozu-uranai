d4a8441a5c7afcd2f6054971560799dc1f9fde87
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-05-22 21:02:50 +0900

    feat: 月支と干支を追加

--- src/suimei/tokushusei/index.ts
+++ src/suimei/tokushusei/index.ts
@@ -1,6 +1,7 @@
 import { Kanshi } from '../Kanshi'
 import 日干と支 from './nikkan-shi'
 import 日干と日支 from './nikkan-nisshi'
+import 月支と干支 from './gesshi-kanshi'
 
 export const tokushusei = (kanshi: Kanshi) => {
   const tokushuseiList: string[] = []
@@ -12,6 +13,12 @@ export const tokushusei = (kanshi: Kanshi) => {
   })
   Object.entries(日干と日支).forEach(([name, rule]) => {
     if (rule[kanshi.日干].includes(kanshi.日支)) {
+      tokushuseiList.push(name)
+    }
+  })
+  Object.entries(月支と干支).forEach(([name, rule]) => {
+    const tenkanChishi = [...kanshi.天干, ...kanshi.地支]
+    if (tenkanChishi.some((shi) => rule[kanshi.月支].includes(shi))) {
       tokushuseiList.push(name)
     }
   })
--- test/suimei/Tokushusei.test.ts
+++ test/suimei/Tokushusei.test.ts
@@ -2,10 +2,24 @@ import { getKanshiInstance } from './test-util'
 import { tokushusei } from '../../src/suimei'
 
 describe('Tokushusei', () => {
-  it('日干と支、日干と日支', async () => {
+  it('日干と支', async () => {
+    const date = new Date('1983-03-15T22:23:00+09:00')
+    const kanshi = await getKanshiInstance(date)
+    const tokushuseiList = tokushusei(kanshi)
+    expect(tokushuseiList).toEqual(expect.arrayContaining(['天官貴人']))
+  })
+
+  it('日干と日支', async () => {
     const date = new Date('2023-03-01T12:00:00+09:00')
     const kanshi = await getKanshiInstance(date)
     const tokushuseiList = tokushusei(kanshi)
-    expect(tokushuseiList).toMatchObject(['羊刃', '日刃'])
+    expect(tokushuseiList).toEqual(expect.arrayContaining(['日刃']))
+  })
+
+  it('月支と干支', async () => {
+    const date = new Date('2023-03-15T11:41:20+09:00')
+    const kanshi = await getKanshiInstance(date)
+    const tokushuseiList = tokushusei(kanshi)
+    expect(tokushuseiList).toEqual(expect.arrayContaining(['天徳貴人']))
   })
 })

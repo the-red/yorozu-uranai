900c32ceee38a692657851ec18b87110f56ec0ca
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-06-05 22:02:37 +0900

    feat: 節入りまでの日数を計算

--- src/suimei/models/SekkiUtil.ts
+++ src/suimei/models/SekkiUtil.ts
@@ -1,6 +1,6 @@
 import { DateTime } from 'luxon'
 import { getEclipticLongitude } from '../../astronomy'
-import { sekki, 節 } from './Sekki'
+import { sekki, 節, 節list } from './Sekki'
 import { SekkiPair } from './Kanshi'
 
 const getSekki = async (date: Date): Promise<節> => {
@@ -15,4 +15,35 @@ export const getSekkiPair = async (date: Date): Promise<SekkiPair> => {
     today: await getSekki(date),
     endOfMonth: await getSekki(dateTime.endOf('month').toJSDate()),
   }
+}
+
+export const daysToNextSekki = async (date: Date, forward: boolean = true): Promise<number> => {
+  const currentSekki = await getSekki(date)
+  const index = 節list.indexOf(currentSekki)
+
+  if (forward) {
+    // 順行
+    const nextIndex = (index + 1) % 12
+    const nextSekki = 節list.at(nextIndex)
+    for (let days = 1; days <= 32; days++) {
+      const nextDate = DateTime.fromJSDate(date).plus({ days }).toJSDate()
+      const sekki = await getSekki(nextDate)
+      if (sekki === nextSekki) {
+        return days
+      }
+    }
+  } else {
+    // 逆行
+    const prevIndex = (index - 1) % 12
+    const prevSekki = 節list.at(prevIndex)
+    for (let days = 1; days <= 32; days++) {
+      const prevDate = DateTime.fromJSDate(date).minus({ days }).toJSDate()
+      const sekki = await getSekki(prevDate)
+      if (sekki === prevSekki) {
+        return days
+      }
+    }
+  }
+
+  throw new Error('節入りできません！') // ここには来ないはず
 }
--- test/suimei/Sekki.test.ts
+++ test/suimei/Sekki.test.ts
@@ -35,3 +35,31 @@ describe('日時→節気 変換', () => {
     expect(sekki(await getEclipticLongitude(new Date('2018-12-07T13:25:55+09:00')))).toEqual('大雪')
   })
 })
+
+describe('節入りまでの日数（日のみ）', () => {
+  it('順行: 立春→30日→啓蟄', async () => {
+    const days = await daysToNextSekki(new Date('2023-02-04T12:00:00+09:00'))
+    expect(days).toBe(30)
+  })
+  it('順行: 小暑→32日→立秋', async () => {
+    const days = await daysToNextSekki(new Date('2023-07-07T20:00:00+09:00'))
+    expect(days).toBe(32)
+  })
+  it('順行: 小寒→1日→立春', async () => {
+    const days = await daysToNextSekki(new Date('2023-02-03T12:00:00+09:00'))
+    expect(days).toBe(1)
+  })
+
+  it('逆行: 啓蟄→30日→立春', async () => {
+    const days = await daysToNextSekki(new Date('2023-03-05T12:00:00+09:00'), false)
+    expect(days).toBe(30)
+  })
+  it('逆行: 立秋→32日→小暑', async () => {
+    const days = await daysToNextSekki(new Date('2023-08-07T20:00:00+09:00'), false)
+    expect(days).toBe(32)
+  })
+  it('逆行: 立春→1日→小寒', async () => {
+    const days = await daysToNextSekki(new Date('2023-02-04T12:00:00+09:00'), false)
+    expect(days).toBe(1)
+  })
+})

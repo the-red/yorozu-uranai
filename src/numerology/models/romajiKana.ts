ad8ac8de76d09a10110af1dfc403419b868c06ef
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-08-03 08:48:57 +0900

    refactor: 関数名変更

--- src/numerology/models/romajiKana.ts
+++ src/numerology/models/romajiKana.ts
@@ -4,4 +4,4 @@ type kana2romaji = {
   [key: string]: string
 }
 
-export const convertHiraganaToRomagi = (target: string): string => hepburn.fromKana(target)
+export const convertKanaToRomaji = (target: string): string => hepburn.fromKana(target)

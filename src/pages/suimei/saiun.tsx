3e04add59f02719a328963505736d5580dec0575
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-08-02 21:09:36 +0900

    feat: 歳運の詳細ページのガワだけ作成

---
+++ src/pages/suimei/saiun.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from 'next'
+
+import { Query } from '../../lib/params'
+
+export type OptionalQuery = Query
+
+const SuimeiSaiunPage: NextPage = () => {
+  return (
+    <div className="suimei-saiun">
+      <div>saiun</div>
+    </div>
+  )
+}
+
+export default SuimeiSaiunPage
